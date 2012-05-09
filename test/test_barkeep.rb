d56035ef2db4cbddbccb62165a5fdd0295a4a3ba
Joseph Rodriguez <jrodriguez@patientslikeme.com>
2012-05-09 09:01:53 -0400

    fix branch link title test

--- test/test_barkeep.rb
+++ test/test_barkeep.rb
@@ -16,13 +16,13 @@ class TestBarkeep < Test::Unit::TestCase
 
   should "render the barkeep bar" do
     stubs(:barkeep_config => {'github_url' => 'http://github.com/project_name', 'panes' => ['branch_info', 'commit_sha_info'], 'environments' => ['development']})
-    GritWrapper.instance.stubs(:repository? => true, :to_hash => {:branch => 'new_branch', :commit => 'abcdef'})
+    GritWrapper.instance.stubs(:repository? => true, :to_hash => {:branch => 'new_branch', :commit => 'abcdef', :last_author => 'Johnny', :date => '2/11/2012'})
     expected = %(
       <dl id="barkeep">
         <dt>Branch:</dt>
         <dd><a href="http://github.com/project_name/tree/new_branch">new_branch</a></dd>
         <dt>Commit:</dt>
-        <dd><a href="http://github.com/project_name/commit/abcdef" title="committed ">abcdef</a></dd>
+        <dd><a href="http://github.com/project_name/commit/abcdef" title="committed 2/11/2012 by Johnny">abcdef</a></dd>
         <dd class="close"><a href="#" onclick="c = document.getElementById('barkeep'); c.parentNode.removeChild(c); return false" title="Close me!">&times;</a></dd>
       </dl>
     )
